cf8f70298b000396a4011065339d4d6bec6f3b3a
kurtthethird123 <90337249+kurtthethird123@users.noreply.github.com>
2024-09-16 14:17:06 -0400

    v0.1

---
+++ 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Template {
+  
+  static void Main() {
+
+  int rock = R;
+  int paper = P;
+  int scissors = S;
+
+  Console.Writeline("Hello and welcome to the game!!!\n");
+  Console.Writeline("Please choose from the following\n");
+
+  for (int rock = 5; int paper = 5; int scissors = 5)
+  {
+    if (rock < paper)
+    {
+      Console.WriteLine("Paper beats Rock\n");
+    }
+    else if (rock > scissors)
+    {
+      Console.WriteLine("Rock beats Scissors");
+    }
+    else if (scissors < paper)
+    
+  }
+
+  str CPUChoice = ""
+  str NumChoices = ""
+  str PlayerChoice = ""
+
+
+  }
+}
+  
